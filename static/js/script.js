6bb46204af6368046f2b67ac473ca31d958d9893
Hazwan <231215h@mymail.nyp.edu.sg>
2025-07-11 15:14:09 +0800

    Initial Commit

--- static/js/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const buttons = document.querySelectorAll(".accordion-btn, .toggle-header");
-    buttons.forEach(btn => {
-        btn.addEventListener("click", () => {
-            const content = btn.nextElementSibling;
-            content.style.display = content.style.display === "block" ? "none" : "block";
-        });
-    });
-});
